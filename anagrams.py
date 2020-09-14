bf8650aba0775016e1d52990673af7aae8586f8f
Carmelly212 <71250144+Carmelly212@users.noreply.github.com>
2020-09-14 09:53:35 +0300

    Add files via upload

---
+++ anagrams.py
@@ -0,0 +1,22 @@
+list_of_words = ['deltas', 'retainers', 'desalt', 'pants', 'slated', 'ternaries', 'smelters', 'termless', 'salted', 'staled', 'greatening', 'lasted', 'resmelts']
+
+
+anagrams = []
+temp_anagrams = []
+singles = []
+
+for word in list_of_words:
+     temp_anagrams.clear()
+     temp_anagrams.append(sorted(word))
+for word_2 in list_of_words:
+    if sorted(word_2) in temp_anagrams:
+       anagrams.append(word_2)
+    else:
+       singles.append([word_2])
+
+#new = singles + [anagrams]
+
+print (anagrams)
+
+
+
